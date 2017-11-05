70460cebb353f13739fc378a2ef6529134b5d44c
safamoh <safa-email@hotamil.com>
2017-11-05 13:35:06 +0300

    Added a comment

--- app/src/main/java/com/example/gis/googlemapsgis/MapsActivity.java
+++ app/src/main/java/com/example/gis/googlemapsgis/MapsActivity.java
@@ -62,6 +62,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        //Comment from Safa
         mMap = googleMap;
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
